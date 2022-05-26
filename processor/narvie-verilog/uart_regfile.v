1cbbff7737bcf782a6ea8be257af2373fd3b868c
Antros Economides <ae457@cam.ac.uk>
2022-05-26 20:30:38 +0100

    Make uart_regfile synthesizable

--- processor/narvie-verilog/uart_regfile.v
+++ processor/narvie-verilog/uart_regfile.v
@@ -1,6 +1,3 @@
-//top.v
-//Top level entity, linking cpu with data and instruction memory
-
 module uart_regfile(
 			clk12,
 			tx,
@@ -19,7 +16,7 @@ module uart_regfile(
 	output tx;
 	input rx;
 	output clk_proc;
-    output[31:0] inst_out;
+	output[31:0] inst_out;
 	input regfile_do_write;
 	input [4:0] regfile_write_addr;
 	input [31:0] regfile_write_data;
@@ -30,13 +27,13 @@ module uart_regfile(
 
 
 	localparam noop = 32'h13000000;
-    localparam SENDING       = 2'b00;
-    localparam READY         = 2'b01;
-    localparam ABOUT_TO_SEND = 2'b11;
+	localparam SENDING       = 2'b00;
+	localparam READY         = 2'b01;
+	localparam ABOUT_TO_SEND = 2'b11;
 
-    reg[1:0] state = READY;
+	reg[1:0] state = READY;
 	reg[7:0] index;
-    reg[7:0] next_index;
+	reg[7:0] next_index;
 
 	reg[31:0] regfile[31:0];
 	reg send_regfile = 0;
@@ -46,7 +43,8 @@ module uart_regfile(
 	reg rstn = 0;
 
 	wire tx_ready;
-    reg [7:0] tx_data;
+	reg [31:0] tx_register;
+	wire [7:0] tx_data;
 	wire [31:0] instruction_buffer;
 	wire instruction_rcv;
 
@@ -63,19 +61,31 @@ module uart_regfile(
 		if(regfile_do_write == 1 && regfile_write_addr != 0) begin
 			regfile[regfile_write_addr] <= regfile_write_data;
 		end
+
 		regfile_read_data0 <= regfile[regfile_read_address0];
 		regfile_read_data1 <= regfile[regfile_read_address1];
 	end
 
 	always @(posedge clk12)
-		tx_data <= regfile[next_index[6:2]][8 * next_index[1:0] +: 8];
+		tx_register <= regfile[next_index[6:2]];
+
+	always @* begin
+		case (next_index[1:0])
+			2'b00 : tx_data = tx_register[31:24];
+			2'b01 : tx_data = tx_register[7:0];
+			2'b10 : tx_data = tx_register[15:8];
+			2'b11 : tx_data = tx_register[23:16];
+		endcase
+	end
 
 	always @(posedge clk12) begin
 		rstn <= 1;
+
 		if (instruction_rcv == 1 && do_execute == 0) begin
 			proc_cycle_count <= 0;
 			do_execute <= 1;
 		end
+
 		if (do_execute == 1) begin
 			if (proc_cycle_count[3:1] == 4) begin
 				do_execute <= 0;
@@ -83,32 +93,33 @@ module uart_regfile(
 			end
 			proc_cycle_count <= proc_cycle_count + 1;
 		end
+
 		if (send_regfile == 1) begin
 			send_regfile <= 0;
 		end
 	end
 
-    always @(posedge clk12) begin
-        if (send_regfile == 1 && state == READY) begin
-            state <= ABOUT_TO_SEND;
-            index <= 0;
-            next_index <= 1;
-        end
+	always @(posedge clk12) begin
+		if (send_regfile == 1 && state == READY) begin
+			state <= ABOUT_TO_SEND;
+			index <= 0;
+			next_index <= 1;
+		end
 
-        if (state == ABOUT_TO_SEND && tx_ready == 0) begin
-            state <= SENDING;
-        end
+		if (state == ABOUT_TO_SEND && tx_ready == 0) begin
+			state <= SENDING;
+		end
 
-        if (tx_ready == 1 && state == SENDING) begin
-            if (index == 127)
-                state <= READY;
-            else
-                state <= ABOUT_TO_SEND;
+		if (tx_ready == 1 && state == SENDING) begin
+			if (index == 127)
+				state <= READY;
+			else
+				state <= ABOUT_TO_SEND;
 
-            index <= next_index;
-            next_index <= next_index + 1;
-        end
-    end
+			index <= next_index;
+			next_index <= next_index + 1;
+		end
+	end
 
 	rx_instruction RX0 (
 		.clk12(clk12),
@@ -119,11 +130,11 @@ module uart_regfile(
 	);
 
 	uart_tx #(.BAUDRATE(`B115200)) TX0 (
-        .clk(clk12),
-        .rstn(rstn),
-        .data(tx_data),
-        .start(state == ABOUT_TO_SEND),
-        .tx(tx),
-        .ready(tx_ready)
-    );
+		.clk(clk12),
+		.rstn(rstn),
+		.data(tx_data),
+		.start(state == ABOUT_TO_SEND),
+		.tx(tx),
+		.ready(tx_ready)
+	);
 endmodule
